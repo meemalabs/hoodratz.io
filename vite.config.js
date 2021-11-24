d6eaf336e16f3d5a2a6b92ae98b7a4a0207380a9
Harlequin Doyon <harlequin.doyon@gmail.com>
2021-11-24 10:50:24 +0800

    Support custom port

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ command, mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    open: true,
-  },
+    server: {
+      open: true,
+      port: process.env.VITE_APP_PORT || 3000
+    },
+  }
 })
