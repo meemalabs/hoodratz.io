a5850c60d093c02e98cbf45de03b19b9f52d781d
Harlequin Doyon <harlequin.doyon@gmail.com>
2021-11-24 18:07:12 +0800

    Adjusted the header to show the overlapping text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="bg-purple-900 w-screen h-full text-gray-400">
-    <header class="bg-purple-800 h-16 flex items-center px-6 justify-between">
+<div class="bg-gray-8 w-screen h-full text-gray-4">
+    <header class="bg-gray-6 h-16 flex items-center px-6 justify-between">
         <div class="w-32"><img src="/images/logo.svg" alt=""></div>
         <div><img src="/images/menu.svg" alt=""></div>
     </header>
@@ -10,15 +10,21 @@
     <!-- Content -->
     <div class="mx-4">
         <div class="mb-8">
-            <h1 class="text-pink-500 uppercase font-sora mt-8">About</h1>
-            <h2 class="text-white font-kumbh-bold text-3xl my-4">The Hoodratz story</h2>
+            <div class="relative">
+                <div class="w-64 -ml-4"><img src="/images/about-txt.svg" alt="About"></div>
+                <h1 class="text-pink-3 uppercase font-sora mt-8 absolute inset-0 tracking-widest">About</h1>
+            </div>
+            <h2 class="text-white font-kumbh-bold text-3xl mb-4">The Hoodratz story</h2>
             <p class="font-sora leading-7">
                 Hoodratz are uniquely generated, cute and collectible rats with proof of ownership stored on the Solana blockchain. All individual pieces of the Hoodratz world have been created by hand and algorithmically generated using custom code.  There are 165 attributes across 18 categories: These categories include background, body, type, pattern, tail, expression, mouth, eyes, skin, glasses, facial hair, hat/hair, mouth accessories, earring, neck, blankets, flags and best of all, cigars! With these combined, it gives a total mathematical combination of millions of unique Hoodratz - but with only 10,000 being minted, some are more unique and rare than others.
             </p>
         </div>
 
         <div>
-            <h1 class="text-pink-500 uppercase font-sora mt-8">The Team</h1>
+            <div class="relative">
+                <div class="-ml-4 absolute inset-x-0 top-0"><img src="/images/the-team.svg" alt="The Team"></div>
+                <h1 class="text-pink-3 uppercase font-sora mt-8 tracking-widest">The Team</h1>
+            </div>
             <h2 class="text-white font-kumbh-bold text-3xl my-4">Who's behind it all?</h2>
             <p class="font-sora leading-7">
                 We are a small group of friends with the same vision and goal. Our core principles have always been to deliver the best possible NFT user experience from launch as well as creating collectible art with utility.
@@ -26,7 +32,7 @@
         </div>
 
         <div class="mt-12 mb-20">
-            <div class="flex bg-purple-700 my-6 px-8 py-6 rounded-4xl">
+            <div class="flex bg-gray-7 my-6 px-8 py-6 rounded-4xl">
                 <div class="w-16 mr-4">
                     <img src="/images/team/chris.png" alt="Chris Breuer">
                 </div>
@@ -39,7 +45,7 @@
                     <img src="/images/linkedin.svg" alt="Linkedin">
                 </div>
             </div>
-            <div class="flex bg-purple-700 my-6 px-8 py-6 rounded-4xl">
+            <div class="flex bg-gray-7 my-6 px-8 py-6 rounded-4xl">
                 <div class="w-16 mr-4">
                     <img src="/images/team/avery.png" alt="Chris Breuer">
                 </div>
@@ -52,7 +58,7 @@
                     <img src="/images/linkedin.svg" alt="Linkedin">
                 </div>
             </div>
-            <div class="flex bg-purple-700 my-6 px-8 py-6 rounded-4xl">
+            <div class="flex bg-gray-7 my-6 px-8 py-6 rounded-4xl">
                 <div class="w-16 mr-4">
                     <img src="/images/team/chris.png" alt="Chris Breuer">
                 </div>
@@ -65,7 +71,7 @@
                     <img src="/images/linkedin.svg" alt="Linkedin">
                 </div>
             </div>
-            <div class="flex bg-purple-700 my-6 px-8 py-6 rounded-4xl">
+            <div class="flex bg-gray-7 my-6 px-8 py-6 rounded-4xl">
                 <div class="w-16 mr-4">
                     <img src="/images/team/chris.png" alt="Chris Breuer">
                 </div>
@@ -81,10 +87,10 @@
         </div>
     </div>  
 
-    <footer class="text-gray-300 font-sora">
+    <footer class="text-gray-3 font-sora">
         <div>
-            <h3 class="bg-purple-700 text-white uppercase px-4 py-3 text-sm">Company</h3>
-            <ul class="divide-y divide-purple-700 text-xs">
+            <h3 class="bg-gray-7 text-white uppercase px-4 py-3 text-sm">Company</h3>
+            <ul class="divide-y divide-gray-7 text-xs">
                 <li class="p-4 flex items-center justify-between">
                     <div>About</div>
                     <div><img src="/images/chevron.svg" alt=""></div>
@@ -94,8 +100,8 @@
                     <div><img src="/images/chevron.svg" alt=""></div>
                 </li>
             </ul>
-            <h3 class="bg-purple-700 text-white uppercase px-4 py-3 text-sm">Hoodratz</h3>
-            <ul class="divide-y divide-purple-700 text-xs">
+            <h3 class="bg-gray-7 text-white uppercase px-4 py-3 text-sm">Hoodratz</h3>
+            <ul class="divide-y divide-gray-7 text-xs">
                 <li class="p-4 flex items-center justify-between">
                     <div>Attributes</div>
                     <div><img src="/images/chevron.svg" alt=""></div>
@@ -105,8 +111,8 @@
                     <div><img src="/images/chevron.svg" alt=""></div>
                 </li>
             </ul>
-            <h3 class="bg-purple-700 text-white uppercase px-4 py-3 text-sm">Policies</h3>
-            <ul class="divide-y divide-purple-700 text-xs">
+            <h3 class="bg-gray-7 text-white uppercase px-4 py-3 text-sm">Policies</h3>
+            <ul class="divide-y divide-gray-7 text-xs">
                 <li class="p-4 flex items-center justify-between">
                     <div>Privay policy</div>
                     <div><img src="/images/chevron.svg" alt=""></div>
@@ -117,7 +123,7 @@
                 </li>
             </ul>
         </div>
-        <div class="bg-purple-800 py-8 px-3 font-sora text-sm">
+        <div class="bg-gray-6 py-8 px-3 font-sora text-sm">
             <div class="flex justify-center my-6">
                 <img src="/images/logo-light.svg" alt="Hoodratz">
             </div>
@@ -126,18 +132,18 @@
             </p>
             <div class="flex justify-center mb-4">
                 <ul class="flex">
-                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-500">
+                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-5">
                         <img src="/images/discord.svg" alt="Discord">
                     </li>
-                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-500">
+                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-5">
                         <img src="/images/twitter.svg" alt="Twitter">
                     </li>
-                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-500">
+                    <li class="mx-4 rounded-full border p-4 flex items-center justify-center border-gray-5">
                         <img src="/images/insta.svg" alt="Instagram">
                     </li>
                 </ul>
             </div>
-            <div class="text-center text-gray-400">
+            <div class="text-center text-gray-4">
                 <small>© 2021 Hoodratz Inc. All right reserved</small>
             </div>
         </div>
