b4a21a8c7ae97757beeda3ca7f2bcfff136dfc7e
Sleepy Monkey <manalili123@gmail.com>
2021-11-24 15:09:27 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,26 @@
     <section id="hero" class="relative min-h-screen bg-cover sm:bg-hero bg-hero-sm">
       <img class="hidden sm:block" src="/images/home/Hoodratz.svg" alt="">
       
-      <div class="absolute flex items-center justify-center w-full top-24">
-        <div class="relative mr-20">
-          <h1 class="font-bold text-white kumbh-bold" style="font-size: 68px; line-height: 90px; letter-spacing: 1%">Discover HoodRatz, <br>a new NFT collection</h1>
-          <img class="absolute -top-5" src="/images/home/Hoodratz-discover.svg" alt="">
-          <p class="mt-10 font-sora text-hoodratz-grey-2" style="font-size: 20px; line-height: 30px; letter-spacing: 1%; font-weight: 400">
+      <div class="absolute flex items-center justify-center w-full top-36">
+        <div class="relative mr-12">
+          <h1 class="relative z-10 font-bold text-white kumbh-bold" style="font-size: 68px; line-height: 90px; letter-spacing: 1%">Discover HoodRatz, <br>a new NFT collection</h1>
+          <img class="absolute -top-10" src="/images/home/Hoodratz-discover.svg" alt="">
+          <p class="relative mt-10 font-sora text-hoodratz-grey-2" style="font-size: 20px; line-height: 30px; letter-spacing: 1%; font-weight: 400">
             A limited edition of 10.000 collectible pixel rats.
           </p>
           <div class="relative flex items-center top-10">
             <button class="py-2 mr-5 bg-white rounded-full px-7">JOIN DISCORD</button>
             <button class="py-2 bg-white rounded-full px-7">FOLLOW US</button>
           </div>
+          <div class="relative z-50 flex items-center text-white top-80">
+            <p class="text-hoodratz-grey-4">Launching on <span class="text-hoodratz-pink-3">Dec 24!</span></p>
+          </div>
         </div>
         <div>
           <img src="/images/home/Hoodratz-card.png" alt="" style="width: 606.12px;height: 738.95px;">
         </div>
       </div>
+      <img class="absolute" src="/images/home/Hoodratz-home-footer.png" alt="">
     </section>
     asdasd
   </div>
